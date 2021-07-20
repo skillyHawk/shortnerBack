1cf98b7ffeec3ab09692c2d446d406c5f59f449b
SAFRAN <remyy.brichet@gmail.com>
2021-07-20 10:41:59 +0200

    sql modified

--- bitly_back/mysql_dump/bitly.sql
+++ bitly_back/mysql_dump/bitly.sql
@@ -1,21 +1,88 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db
+-- Généré le : mar. 20 juil. 2021 à 08:37
+-- Version du serveur : 8.0.25
+-- Version de PHP : 7.4.21
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Table `bitly`.`User`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bitly`.`User` (
-  `idUser` INT(11) NOT NULL AUTO_INCREMENT,
-  `pseudo` VARCHAR(45) NOT NULL UNIQUE,
-  `password` VARCHAR(90) NOT NULL,
-  PRIMARY KEY (`idUser`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bitly`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `link`
+--
+
+CREATE TABLE `link` (
+  `idLink` int NOT NULL,
+  `longLink` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `shortLink` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `qrCode` varchar(255) NOT NULL,
+  `idUser` int DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `dateLink` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `User`
+--
+
+CREATE TABLE `User` (
+  `idUser` int NOT NULL,
+  `pseudo` varchar(45) NOT NULL,
+  `password` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `link`
+--
+ALTER TABLE `link`
+  ADD PRIMARY KEY (`idLink`);
+
+--
+-- Index pour la table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `pseudo` (`pseudo`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `link`
+--
+ALTER TABLE `link`
+  MODIFY `idLink` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `User`
+--
+ALTER TABLE `User`
+  MODIFY `idUser` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
